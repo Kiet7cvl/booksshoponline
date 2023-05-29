6ba137787d172102f03bda9581f0797f0e662a5e
Do lam <lamdthe16385@fpt.edu.vn>
2023-05-29 20:12:27 +0700

    edit home and add product detail

--- BooksShoppingOnline/src/java/dal/ProductDAO.java
+++ BooksShoppingOnline/src/java/dal/ProductDAO.java
@@ -16,7 +16,8 @@ import model.Product;
  *
  * @author ADMIN
  */
-public class ProductDAO extends DBContext{
+public class ProductDAO extends DBContext {
+
     public List<Product> get4ProductRandom() {
         List<Product> list = new ArrayList<>();
         String sql = "SELECT * FROM Product ORDER BY RAND() LIMIT 4;";
@@ -47,6 +48,7 @@ public class ProductDAO extends DBContext{
         }
         return list;
     }
+
     public String getImgProduct(int id) {
         String sql = "select images from Products_Images where product_id = ?";
         try {
@@ -61,6 +63,7 @@ public class ProductDAO extends DBContext{
         }
         return null;
     }
+
     public double getRatedProduct(int id) {
         String sql = "select AVG(rated_star) from Feedback where product_id = ?";
         try {
@@ -74,5 +77,72 @@ public class ProductDAO extends DBContext{
             System.out.println(e);
         }
         return 0;
+    }
+    
+    public static void main(String[] args) {
+//        System.out.println(new ProductDAO().getProductById(2));
+    }
+    
+    public Product getProductById(int productId) {
+        String sql = "select * from Product where product_id = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productId);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Product p = Product.builder()
+                        .id(rs.getInt(1))
+                        .name(rs.getString(2))
+                        .original_price(rs.getInt(3))
+                        .sale_price(rs.getInt(4))
+                        .desciption(rs.getString(5))
+                        .brief_infor(rs.getString(6))
+                        .status(rs.getBoolean(7))
+                        .quantity(rs.getInt(8))
+                        .category_id(rs.getInt(9))
+                        .update_date(rs.getDate(10))
+                        .image(getImgProduct(rs.getInt(1)))
+                        .rated_star(getRatedProduct(rs.getInt(1)))
+                        .build();
+
+                return p;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+
+    }
+    
+    public List<Product> getProductTop4Category(int productId, int categoryId) { //getProductTop4Category Ramdom
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Product WHERE category_id = ? AND product_id != ? ORDER BY RAND() LIMIT 4;";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, categoryId);
+            st.setInt(2, productId);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Product p = Product.builder()
+                        .id(rs.getInt(1))
+                        .name(rs.getString(2))
+                        .original_price(rs.getInt(3))
+                        .sale_price(rs.getInt(4))
+                        .desciption(rs.getString(5))
+                        .brief_infor(rs.getString(6))
+                        .status(rs.getBoolean(7))
+                        .quantity(rs.getInt(8))
+                        .category_id(rs.getInt(9))
+                        .update_date(rs.getDate(10))
+                        .image(getImgProduct(rs.getInt(1)))
+                        .rated_star(getRatedProduct(rs.getInt(1)))
+                        .build();
+
+                list.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
     }
 }
