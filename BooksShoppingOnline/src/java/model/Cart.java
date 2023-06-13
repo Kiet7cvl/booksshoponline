4d9cb94935b041c3f2224876059777f397589e78
truongbqhe160611 <buitruong091102@gmail.com>
2023-06-13 21:26:16 +0700

    Cart Detail and Cart contact

--- BooksShoppingOnline/src/java/model/Cart.java
+++ BooksShoppingOnline/src/java/model/Cart.java
@@ -1,27 +1,28 @@
 /*
-<<<<<<< HEAD
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-=======
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
->>>>>>> 248f8f1faa46c4245d5792a118433fa629bcf5f9
  */
+
 package model;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-
+/**
+ *
+ * @author dongh
+ */
 @Builder
 @Getter
 @Setter
 @ToString
-public class Category {
-    private int id;
-    private String name;
-    
+public class Cart {
+        private int cart_id;
+        private int product_id;
+        private String product_name;
+        private int product_price;
+        private int quantity;
+        private int total_cost;
+        private int user_id;              
 }
